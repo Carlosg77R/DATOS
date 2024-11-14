4b5b92d82da165fe740377517d7617af27f6fd4c
AMO125 <angelmo@ciencias.unam.mx>
2024-11-13 23:18:14 -0600

    Update ddl_metro.sql

--- ddl_metro.sql
+++ ddl_metro.sql
@@ -1,3 +1,10 @@
+--Creación de tablas
+/*Dudas:
+La tabla estación tiene int en numeroestacion
+La tabla teléfono tiene dos pk
+
+*/
+
 CREATE table Estación(
 NúmeroEstación int,
 JefeEstación varchar(150) not null,
@@ -6,34 +13,44 @@ HoraApertura Time not null,
 HoraCierre Time not null,
 Teléfono char(10) not null,
 constraint pkEstación primary key(NúmeroEstación)
+constraint check(NúmeroEstación between 1 and 12)
 );
 
 CREATE table Acceso(
 NúmeroAcceso int not null,
 NúmeroEstación int not null,
-HoraApertura time not null,
-HorraCierre time not null,
-constraint pkAcceso primary key(NúmeroAcceso)
+HoraApertura time not null DEFAULT 5:00:00.000,
+HorraCierre time not null DEFAULT 0:00:00.000,
+constraint pkAcceso primary key(NúmeroAcceso),
 constraint fkAcceso foreign key(NúmeroEstación) references Estación(NúmeroEstación)
+on update cascade on delete cascade
+constraint check(NúmeroAcceso >0)
+
 );
 
 CREATE table Servicio(
 IDServicio int not null,
 Nombre varchar(100) not null,
-Tipo varchar(100) not null
+Tipo varchar(100) not null,
 constraint pkServicio primary key(IDServicio)
+constraint check(IDServicio >0)
 );
 
 CREATE table Conectar(
-NúmeroEstación int,
-IDServicio int,
+NúmeroEstación int not null,
+IDServicio int not null,
 constraint fk1Conectar foreign key(NúmeroEstación) references Estación(NúmeroEstación)
+on update cascade on delete cascade,
 constraint fk2Conectar foreign key(IDServicio) references Servicio(IDServicio)
+on update cascade on delete cascade
 );
 
 CREATE table Hangar(
-IDHangar int,
-NúmeroEstación int,
+IDHangar int not null,
+NúmeroEstación int not null,
 constraint pkHangar primary key (IDHangar),
 constraint fkHangar foreign key(NúmeroEstación) references Estación(NúmeroEstación)
+on update cascade on delete cascade
+constraint check(IDHangar >0)
 );
+
